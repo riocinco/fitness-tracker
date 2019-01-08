a27fcaf55be5fa0b19b6dc31774c309f5334fd05
riocinco <rioecinco@yahoo.com>
2019-01-08 13:31:07 +0800

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCZUIbmp9fdbYyWIiQ6l7v0-9dtgzcwAE8',
+    authDomain: 'fitness-tracker-b7f43.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-b7f43.firebaseio.com',
+    projectId: 'fitness-tracker-b7f43',
+    storageBucket: 'fitness-tracker-b7f43.appspot.com',
+    messagingSenderId: '841132057832'
+  }
 };
